c66d7e958e8e360d282aba30c303c9b8347a6e1c
Maxim Filimonov <tpaktopsp@gmail.com>
2017-04-08 15:51:45 +0800

    add play and stop button

--- main.js
+++ main.js
@@ -9,36 +9,59 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity
 } from 'react-native';
 
 const requireAudio = require('./advertising.mp3');
 const Sound = require('react-native-sound');
+const Button = ({ title, onPress, style }) => (
+  <TouchableOpacity onPress={onPress}>
+    <Text style={style}>{title}</Text>
+  </TouchableOpacity>
+);
+
 
 export default class WaveyApp extends Component {
-  render() {
-    // const s = new Sound(requireAudio, (e) => {
-    //   if (e) {
-    //     console.log('error', e);
-    //     return;
-    //   }
+  constructor(props) {
+    super(props);
+    this.state = {
+      sound: undefined,
+      playing: false
+    }
+  }
+  onPlay = (e) => {
+    if (!this.state.playing) {
+      sound = new Sound(requireAudio, (e) => {
+        if (e) {
+          console.log('error', e);
+          return;
+        }
 
-    //   s.play(() => s.release());
-    // })
+        sound.play(() => sound.release());
+      })
+      this.setState({ sound: sound, playing: true });
+    }
+  }
+  onStop = (e) => {
+    var sound = this.state.sound;
+    sound.stop(() => sound.release());
+    this.setState({
+      playing: false
+    })
+  }
+  render() {
+    var playingLabel = "nothing is playing";
+    if (this.state.playing) {
+      playingLabel = "playing....";
+    }
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Button style={[styles.button, styles.playButton]} title="Play" onPress={this.onPlay} />
+        <Button style={[styles.button, styles.stopButton]} title="Stop" onPress={this.onStop} />
+        <Text style={styles.playingLabel}>{playingLabel}</Text>
       </View>
-    );
+    )
   }
 }
 
@@ -49,16 +72,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
+  playingLabel: {
+    fontSize: 40,
     margin: 10,
+    color: "orange"
   },
-  instructions: {
+  button: {
+    fontWeight: "bold",
+    borderWidth: 2,
+    padding: 5,
+    borderRadius: 10,
+    width: 100,
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    margin: 4
   },
+  playButton: {
+    color: "red",
+  },
+  stopButton: {
+    color: 'gray',
+  }
 });
 
 AppRegistry.registerComponent('WaveyApp', () => WaveyApp);
